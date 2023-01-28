e4f87f8110b0975eacc5d2a49d740facb5f0e84e
rbrennan <rory.brennan@genesys.com>
2023-01-28 16:48:01 +0000

    Add humidity and resolved city, more code checks, return metrics

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ url = f"http://api.openweathermap.org/data/2.5/weather?appid={API_Key}"
 @app.route('/weather', methods=['GET'])
 def weather():
     locale = request.args.get('locale')
-    response = requests.get(url + "&q=" + locale)
+    response = requests.get(url + "&q=" + locale + "&units=metric")
     res = response.json()
 
     app.logger.info(res)
 
     # Checking if the city is found
-    if res["cod"] != "404" or res["cod"] != "400":
+    if res["cod"] != "404" or res["cod"] != "400" or res["cod"] != "500":
         data = res["main"]
 
         # Storing the live temperature data
@@ -25,14 +25,19 @@ def weather():
 
         # Storing the live pressure data
         live_pressure = data["pressure"]
-        desc = res["weather"]
+
+        # Storing the humidity
+        live_humidity = data["humidity"]
 
         # Storing the weather description
+        desc = res["weather"]
         weather_description = desc[0]["description"]
 
         return {
+            "name": res['name'],
             "temp": live_temperature,
             "pressure": live_pressure,
+            "humidity": live_humidity,
             "desc": weather_description
         }
 
